19cb29885bff8d67f93b01e325a411e68b05db57
Karlaleticiaavila <Kbliaass@gmail.com>
2025-11-19 19:05:13 -0800

    exercise callbacks

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,25 @@
 const fs = require("fs");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+fs.readFile("./firstname.txt", "utf-8", (err, firstname) => {
+  if (err) return console.log(err);
+
+  fs.readFile("./lastname.txt", "utf-8", (err, lastname) => {
+    if (err) return console.log(err);
+
+    fs.readFile("./age.txt", "utf-8", (err, age) => {
+      if (err) return console.log(err);
+
+      fs.readFile("./hobbies.txt", "utf-8", (err, hobbiesData) => {
+        if (err) return console.log(err);
+
+        // Convert hobbies to array 
+        const hobbies = hobbiesData.split(",").map(h => h.trim());
+
+        // final sentence
+        const sentence = `${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}.`;
+
+        console.log(sentence);
+      });
+    });
+  });
+});
